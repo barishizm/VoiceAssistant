0063a05fafe66eeb71366546a70bb5eb8b46ce9e
ToDoList <mbakinti@outlook.com>
2024-06-09 14:28:26 +0300

    Speak audio and taking the command.

--- Riva.py
+++ Riva.py
@@ -3,3 +3,29 @@ import speech_recognition as sr
 import wikipedia
 import webbrowser
 import os
+
+# pyttsx
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+
+engine.setProperty('voice', voices[1].id)  # 1 for female and 0 for male voice.
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def take_command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print("User said:" + query + "\n")
+    except Exception as e:
+        print(e)
+        speak("I didnt understand")
+        return "None"
+    return query
